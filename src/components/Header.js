e682f5bc68d14a487ad93a11eb394166543ff0d5
gcerv93 <gcerv93@protonmail.com>
2022-03-30 11:00:03 -0500

    Add initial Header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/main.css";
+
+class Header extends React.Component {
+  render() {
+    return <header>CV Maker</header>;
+  }
+}
+
+export default Header;
