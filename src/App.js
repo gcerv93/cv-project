42c9ae63533846b265a7baf9398953c90ff4acf8
gcerv93 <gcerv93@protonmail.com>
2022-03-30 11:47:34 -0500

    Add Footer
    There's also a content component that fills the rest of the page so that
    the footer will sit at the bottom of the page. The div is empty for now
    but will filled in later.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Content />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/main.css";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>
+          Created by{" "}
+          <a href="https://github.com/gcerv93" target="_blank" rel="noreferrer">
+            gcerv93
+          </a>
+        </p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
