a75bda78c5509ccf94d9569531a7ad38693689bc
gcerv93 <gcerv93@protonmail.com>
2022-04-05 15:17:20 -0500

    Add static experience section

---
+++ src/components/Experience.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Experience extends React.Component {
+  render() {
+    return (
+      <div className="experience">
+        <h2>Practical Experience</h2>
+        <button type="button">Add More</button>
+      </div>
+    );
+  }
+}
+
+export default Experience;
--- src/components/Info.js
+++ src/components/Info.js
@@ -2,6 +2,7 @@ import React from "react";
 import InfoForm from "./InfoForm";
 import Skills from "./Skills";
 import Education from "./Education";
+import Experience from "./Experience";
 
 class Info extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class Info extends React.Component {
         <InfoForm />
         <Skills />
         <Education />
+        <Experience />
       </div>
     );
   }
