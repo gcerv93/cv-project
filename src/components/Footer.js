64d052a1cc98cc6ef2adcef752c2e735261dbf1f
gcerv93 <gcerv93@protonmail.com>
2022-04-10 01:56:00 -0500

    Add Github img to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/main.css";
+import github from "../images/github.svg";
 
 class Footer extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class Footer extends React.Component {
           Created by{" "}
           <a href="https://github.com/gcerv93" target="_blank" rel="noreferrer">
             gcerv93
+            <img src={github} alt="github" height="16" width="16" />
           </a>
         </p>
       </footer>
