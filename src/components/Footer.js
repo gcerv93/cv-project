5a1a9bfadfaaa7d20cc355c5ee6615a63aee021d
gcerv93 <gcerv93@protonmail.com>
2022-04-21 02:21:19 -0500

    Change Footer to a functional component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,18 @@ import React from "react";
 import "../styles/main.css";
 import github from "../images/github.svg";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer>
-        <p>
-          Created by{" "}
-          <a href="https://github.com/gcerv93" target="_blank" rel="noreferrer">
-            gcerv93
-            <img src={github} alt="github" height="16" width="16" />
-          </a>
-        </p>
-      </footer>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Created by{" "}
+        <a href="https://github.com/gcerv93" target="_blank" rel="noreferrer">
+          gcerv93
+          <img src={github} alt="github" height="16" width="16" />
+        </a>
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
