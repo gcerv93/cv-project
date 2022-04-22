481865d5166069237d86191bb1a9183a9e9b3570
gcerv93 <gcerv93@protonmail.com>
2022-04-22 17:38:37 -0500

    Change Skills to a functional component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,57 +1,45 @@
-import React from "react";
-
-class Skills extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleAddingNewSkill = this.handleAddingNewSkill.bind(this);
-    this.onChanges = this.onChanges.bind(this);
-    this.onRemove = this.onRemove.bind(this);
+const Skills = (props) => {
+  function handleAddingNewSkill() {
+    props.onBtnClick();
   }
 
-  handleAddingNewSkill() {
-    this.props.onBtnClick();
+  function onChanges(id, e) {
+    props.onChanges(id, e);
   }
 
-  onChanges(id, e) {
-    this.props.onChanges(id, e);
+  function onRemove(id, e) {
+    props.onRemove(id, e);
   }
 
-  onRemove(id, e) {
-    this.props.onRemove(id, e);
-  }
-
-  render() {
-    return (
-      <div className="skills">
-        <h2>Skills</h2>
-        {this.props.skills.map((skill, i) => {
-          return (
-            <div className="skill" key={i.toString()}>
-              <label htmlFor="skill">Skill</label>
-              <textarea
-                type="text"
-                id="skills"
-                value={skill.text}
-                onChange={(e) => this.onChanges(skill.id, e)}
-              />
-              <button
-                type="button"
-                className="removeBtn"
-                id="skills"
-                onClick={(e) => this.onRemove(skill.id, e)}
-              >
-                Remove
-              </button>
-            </div>
-          );
-        })}
-        <button type="button" onClick={this.handleAddingNewSkill}>
-          Add more
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="skills">
+      <h2>Skills</h2>
+      {props.skills.map((skill, idx) => {
+        return (
+          <div className="skill" key={idx.toString()}>
+            <label htmlFor="skill">Skill</label>
+            <textarea
+              type="text"
+              id="skills"
+              value={skill.text}
+              onChange={(e) => onChanges(skill.id, e)}
+            />
+            <button
+              type="button"
+              className="removeBtn"
+              id="skills"
+              onClick={(e) => onRemove(skill.id, e)}
+            >
+              Remove
+            </button>
+          </div>
+        );
+      })}
+      <button type="button" onClick={handleAddingNewSkill}>
+        Add more
+      </button>
+    </div>
+  );
+};
 
 export default Skills;
