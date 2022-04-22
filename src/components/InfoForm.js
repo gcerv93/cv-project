07591fe838f831e450a7b7ea9deffa53032d61f0
gcerv93 <gcerv93@protonmail.com>
2022-04-22 18:30:42 -0500

    Change InfoForm to a functional component

--- src/components/InfoForm.js
+++ src/components/InfoForm.js
@@ -1,100 +1,90 @@
-import React from "react";
 import Skills from "./Skills";
 import Education from "./Education";
 import Experience from "./Experience";
 
-class InfoForm extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.onChangePreview = this.onChangePreview.bind(this);
-    this.onChanges = this.onChanges.bind(this);
+const InfoForm = (props) => {
+  function onChanges(id, e) {
+    props.handleInputChanges(id, e);
   }
 
-  onChanges(id, e) {
-    this.props.handleInputChanges(id, e);
-  }
-
-  onChangePreview(e) {
+  function onChangePreview(e) {
     e.preventDefault();
 
-    this.props.handlePreviewChange();
+    props.handlePreviewChange();
   }
 
-  render() {
-    return (
-      <div className="infoForm">
-        <form>
-          <ul>
-            <li>
-              <label htmlFor="firstName">First Name</label>
-              <textarea
-                type="text"
-                name="firstName"
-                value={this.props.firstName.text}
-                onChange={(e) => this.onChanges(null, e)}
-              />
-            </li>
+  return (
+    <div className="infoForm">
+      <form>
+        <ul>
+          <li>
+            <label htmlFor="firstName">First Name</label>
+            <textarea
+              type="text"
+              name="firstName"
+              value={props.firstName.text}
+              onChange={(e) => onChanges(null, e)}
+            />
+          </li>
 
-            <li>
-              <label htmlFor="lastName">Last Name</label>
-              <textarea
-                type="text"
-                name="lastName"
-                value={this.props.lastName.text}
-                onChange={(e) => this.onChanges(null, e)}
-              />
-            </li>
+          <li>
+            <label htmlFor="lastName">Last Name</label>
+            <textarea
+              type="text"
+              name="lastName"
+              value={props.lastName.text}
+              onChange={(e) => onChanges(null, e)}
+            />
+          </li>
 
-            <li>
-              <label htmlFor="email">Email</label>
-              <textarea
-                type="email"
-                name="email"
-                value={this.props.email.text}
-                onChange={(e) => this.onChanges(null, e)}
-              />
-            </li>
+          <li>
+            <label htmlFor="email">Email</label>
+            <textarea
+              type="email"
+              name="email"
+              value={props.email.text}
+              onChange={(e) => onChanges(null, e)}
+            />
+          </li>
 
-            <li>
-              <label htmlFor="phone">Phone</label>
-              <textarea
-                type="text"
-                name="phone"
-                value={this.props.phone.text}
-                onChange={(e) => this.onChanges(null, e)}
-              />
-            </li>
-          </ul>
+          <li>
+            <label htmlFor="phone">Phone</label>
+            <textarea
+              type="text"
+              name="phone"
+              value={props.phone.text}
+              onChange={(e) => onChanges(null, e)}
+            />
+          </li>
+        </ul>
 
-          <Skills
-            skills={this.props.skills}
-            onBtnClick={this.props.addSkillInput}
-            onChanges={this.props.handleInputChanges}
-            onRemove={this.props.handleRemovingInputs}
-          />
+        <Skills
+          skills={props.skills}
+          onBtnClick={props.addSkillInput}
+          onChanges={props.handleInputChanges}
+          onRemove={props.handleRemovingInputs}
+        />
 
-          <Education
-            educations={this.props.educations}
-            onBtnClick={this.props.addEducationInput}
-            onChanges={this.props.handleInputChanges}
-            onRemove={this.props.handleRemovingInputs}
-          />
+        <Education
+          educations={props.educations}
+          onBtnClick={props.addEducationInput}
+          onChanges={props.handleInputChanges}
+          onRemove={props.handleRemovingInputs}
+        />
 
-          <Experience
-            experiences={this.props.experiences}
-            onBtnClick={this.props.addExperienceInput}
-            onChanges={this.props.handleInputChanges}
-            onRemove={this.props.handleRemovingInputs}
-          />
+        <Experience
+          experiences={props.experiences}
+          onBtnClick={props.addExperienceInput}
+          onChanges={props.handleInputChanges}
+          onRemove={props.handleRemovingInputs}
+        />
 
-          <button id="submitBtn" type="submit" onClick={this.onChangePreview}>
-            Preview
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+        <button id="submitBtn" type="submit" onClick={onChangePreview}>
+          Preview
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default InfoForm;
