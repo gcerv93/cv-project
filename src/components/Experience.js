8bfb6530b2a087f6dd9c19dbf48166c2036e3728
gcerv93 <gcerv93@protonmail.com>
2022-04-22 17:57:20 -0500

    Change Experience to a functional component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,105 +1,93 @@
-import React from "react";
-
-class Experience extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleAddingNewExperience = this.handleAddingNewExperience.bind(this);
-    this.onChanges = this.onChanges.bind(this);
-    this.onRemove = this.onRemove.bind(this);
+const Experience = (props) => {
+  function handleAddingNewExperience() {
+    props.onBtnClick();
   }
 
-  handleAddingNewExperience() {
-    this.props.onBtnClick();
+  function onChanges(id, e) {
+    props.onChanges(id, e);
   }
 
-  onChanges(id, e) {
-    this.props.onChanges(id, e);
+  function onRemove(id, e) {
+    props.onRemove(id, e);
   }
 
-  onRemove(id, e) {
-    this.props.onRemove(id, e);
-  }
-
-  render() {
-    return (
-      <div className="experience">
-        <h2>Practical Experience</h2>
-        {this.props.experiences.map((experience, idx) => {
-          return (
-            <div key={idx.toString()}>
-              <div className="experienceItem">
-                <label htmlFor="companyName">Company name</label>
-                <textarea
-                  type="text"
-                  id="experiences"
-                  name="companyName"
-                  value={experience.companyName.text}
-                  onChange={(e) => this.onChanges(experience.id, e)}
-                />
-              </div>
-
-              <div className="experienceItem">
-                <label htmlFor="position">Position</label>
-                <textarea
-                  type="text"
-                  id="experiences"
-                  name="position"
-                  value={experience.position.text}
-                  onChange={(e) => this.onChanges(experience.id, e)}
-                />
-              </div>
-
-              <div className="experienceItem">
-                <label htmlFor="mainTasks">Main tasks</label>
-                <textarea
-                  className="mainTasks"
-                  type="text"
-                  id="experiences"
-                  name="mainTasks"
-                  value={experience.mainTasks.text}
-                  onChange={(e) => this.onChanges(experience.id, e)}
-                />
-              </div>
-
-              <div className="experienceItem">
-                <label htmlFor="startDate">From</label>
-                <input
-                  type="date"
-                  id="experiences"
-                  name="startDate"
-                  value={experience.startDate.text}
-                  onChange={(e) => this.onChanges(experience.id, e)}
-                ></input>
-              </div>
-
-              <div className="experienceItem">
-                <label htmlFor="endDate">To</label>
-                <input
-                  type="date"
-                  id="experiences"
-                  name="endDate"
-                  value={experience.endDate.text}
-                  onChange={(e) => this.onChanges(experience.id, e)}
-                ></input>
-                <button
-                  type="button"
-                  className="removeBtn"
-                  id="experiences"
-                  onClick={(e) => this.onRemove(experience.id, e)}
-                >
-                  Remove
-                </button>
-              </div>
+  return (
+    <div className="experience">
+      <h2>Practical Experience</h2>
+      {props.experiences.map((experience, idx) => {
+        return (
+          <div key={idx.toString()}>
+            <div className="experienceItem">
+              <label htmlFor="companyName">Company name</label>
+              <textarea
+                type="text"
+                id="experiences"
+                name="companyName"
+                value={experience.companyName.text}
+                onChange={(e) => onChanges(experience.id, e)}
+              />
             </div>
-          );
-        })}
-        <button type="button" onClick={this.handleAddingNewExperience}>
-          Add More
-        </button>
-      </div>
-    );
-  }
-}
+
+            <div className="experienceItem">
+              <label htmlFor="position">Position</label>
+              <textarea
+                type="text"
+                id="experiences"
+                name="position"
+                value={experience.position.text}
+                onChange={(e) => onChanges(experience.id, e)}
+              />
+            </div>
+
+            <div className="experienceItem">
+              <label htmlFor="mainTasks">Main tasks</label>
+              <textarea
+                className="mainTasks"
+                type="text"
+                id="experiences"
+                name="mainTasks"
+                value={experience.mainTasks.text}
+                onChange={(e) => onChanges(experience.id, e)}
+              />
+            </div>
+
+            <div className="experienceItem">
+              <label htmlFor="startDate">From</label>
+              <input
+                type="date"
+                id="experiences"
+                name="startDate"
+                value={experience.startDate.text}
+                onChange={(e) => onChanges(experience.id, e)}
+              ></input>
+            </div>
+
+            <div className="experienceItem">
+              <label htmlFor="endDate">To</label>
+              <input
+                type="date"
+                id="experiences"
+                name="endDate"
+                value={experience.endDate.text}
+                onChange={(e) => onChanges(experience.id, e)}
+              ></input>
+              <button
+                type="button"
+                className="removeBtn"
+                id="experiences"
+                onClick={(e) => onRemove(experience.id, e)}
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        );
+      })}
+      <button type="button" onClick={handleAddingNewExperience}>
+        Add More
+      </button>
+    </div>
+  );
+};
 
 export default Experience;
