fe7b2f5e26dcc03e5c9791f1a697703e09da64af
gcerv93 <gcerv93@protonmail.com>
2022-04-22 17:47:12 -0500

    Change Education to a functional component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,93 +1,81 @@
-import React from "react";
-
-class Education extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleAddingNewEducation = this.handleAddingNewEducation.bind(this);
-    this.onChanges = this.onChanges.bind(this);
-    this.onRemove = this.onRemove.bind(this);
+const Education = (props) => {
+  function handleAddingNewEducation() {
+    props.onBtnClick();
   }
 
-  handleAddingNewEducation() {
-    this.props.onBtnClick();
+  function onChanges(id, e) {
+    props.onChanges(id, e);
   }
 
-  onChanges(id, e) {
-    this.props.onChanges(id, e);
+  function onRemove(id, e) {
+    props.onRemove(id, e);
   }
 
-  onRemove(id, e) {
-    this.props.onRemove(id, e);
-  }
-
-  render() {
-    return (
-      <div className="education">
-        <h2>Education</h2>
-        {this.props.educations.map((education, idx) => {
-          return (
-            <div key={idx.toString()}>
-              <div className="educationItem">
-                <label htmlFor="schoolName">School name</label>
-                <textarea
-                  type="text"
-                  id="educations"
-                  name="schoolName"
-                  value={education.schoolName.text}
-                  onChange={(e) => this.onChanges(education.id, e)}
-                />
-              </div>
-
-              <div className="educationItem">
-                <label htmlFor="studyTitle">Title of study</label>
-                <textarea
-                  type="text"
-                  id="educations"
-                  name="studyTitle"
-                  value={education.studyTitle.text}
-                  onChange={(e) => this.onChanges(education.id, e)}
-                />
-              </div>
-
-              <div className="educationItem">
-                <label htmlFor="startDate">From</label>
-                <input
-                  type="date"
-                  id="educations"
-                  name="startDate"
-                  value={education.startDate.text}
-                  onChange={(e) => this.onChanges(education.id, e)}
-                ></input>
-              </div>
-
-              <div className="educationItem">
-                <label htmlFor="endDate">To</label>
-                <input
-                  type="date"
-                  id="educations"
-                  name="endDate"
-                  value={education.endDate.text}
-                  onChange={(e) => this.onChanges(education.id, e)}
-                ></input>
-                <button
-                  type="button"
-                  className="removeBtn"
-                  id="educations"
-                  onClick={(e) => this.onRemove(education.id, e)}
-                >
-                  Remove
-                </button>
-              </div>
+  return (
+    <div className="education">
+      <h2>Education</h2>
+      {props.educations.map((education, idx) => {
+        return (
+          <div key={idx.toString()}>
+            <div className="educationItem">
+              <label htmlFor="schoolName">School name</label>
+              <textarea
+                type="text"
+                id="educations"
+                name="schoolName"
+                value={education.schoolName.text}
+                onChange={(e) => onChanges(education.id, e)}
+              />
             </div>
-          );
-        })}
-        <button type="button" onClick={this.handleAddingNewEducation}>
-          Add More
-        </button>
-      </div>
-    );
-  }
-}
+
+            <div className="educationItem">
+              <label htmlFor="studyTitle">Title of study</label>
+              <textarea
+                type="text"
+                id="educations"
+                name="studyTitle"
+                value={education.studyTitle.text}
+                onChange={(e) => onChanges(education.id, e)}
+              />
+            </div>
+
+            <div className="educationItem">
+              <label htmlFor="startDate">From</label>
+              <input
+                type="date"
+                id="educations"
+                name="startDate"
+                value={education.startDate.text}
+                onChange={(e) => onChanges(education.id, e)}
+              ></input>
+            </div>
+
+            <div className="educationItem">
+              <label htmlFor="endDate">To</label>
+              <input
+                type="date"
+                id="educations"
+                name="endDate"
+                value={education.endDate.text}
+                onChange={(e) => onChanges(education.id, e)}
+              ></input>
+              <button
+                type="button"
+                className="removeBtn"
+                id="educations"
+                onClick={(e) => onRemove(education.id, e)}
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        );
+      })}
+      <button type="button" onClick={handleAddingNewEducation}>
+        Add More
+      </button>
+    </div>
+  );
+};
 
 export default Education;
